cab1fd4cfbf5874709fe88960a5936d760d7e7ef
adepoju2006 <adepojuayomide2006@gmail.com>
2021-05-14 09:39:30 -0700

    first commit

---
+++ oppo.js
@@ -0,0 +1,9 @@
+var winner = prompt("Enter your winner name here");
+var user = prompt("Enter your username here");
+var pass = prompt("Enter your password here");
+
+if ((winner == winner && user == "user104") || pass == "pass") {
+  alert("The name of the winner is" + " " + winner);
+} else {
+  alert("Incorrect Username or Password");
+}
